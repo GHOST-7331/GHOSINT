4353ea353299e7875293bf052e6755046732e308
ɢʜ0sᴛ <59851390+GHOST-7331@users.noreply.github.com>
2024-03-17 11:24:55 -0500

    Update install.py

--- cli/install.py
+++ cli/install.py
@@ -18,7 +18,6 @@ def install_common_requirements():
         "colorama>=0.4.1",
         "certifi>=2019.6.16",
         "PySocks>=1.7.0",
-        "requests>=2.22.0",
         "requests-futures>=1.0.0",
         "stem>=1.8.0",
         "torrequest>=0.1.0",
